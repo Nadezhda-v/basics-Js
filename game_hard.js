d7ba3a695215b2d9d3f6632c1efac359aaa153d1
Nadezhda Vorotova <work-nadezhda-v@yandex.ru>
2023-07-14 10:26:47 +0400

    Оптимизировать код, удалить функцию runGame()

--- game_hard.js
+++ game_hard.js
@@ -1,6 +1,7 @@
 
 
 export let isGameRPSFinished = false;
+let isGameMrcFinished = false;
 
 (() => {
   const FIGURES_ENG = ['rock', 'scissors', 'paper'];
@@ -99,10 +100,7 @@ export let isGameRPSFinished = false;
       const choiceFigures = () => {
         getInputPlayer();
 
-        if (isCancel(inputString)) {
-          isGameRPSFinished = true;
-          return;
-        }
+        if (isCancel(inputString)) return;
 
         if (!isValidInputPlayer(inputString)) {
           choiceFigures();
@@ -112,48 +110,46 @@ export let isGameRPSFinished = false;
         return inputComputer = FIGURES[randomInt];
       };
 
-      const runGame = () => {
-        choiceFigures();
+      choiceFigures();
 
-        if (inputString === null) {
-          return;
-        }
+      if (inputString === null) {
+        isGameRPSFinished = true;
+        return false;
+      }
 
-        let isDrow = false;
+      let isDrow = false;
 
-        while (!isDrow) {
-          if (inputPlayer === inputComputer) {
-            alert(`
+      while (!isDrow) {
+        if (inputPlayer === inputComputer) {
+          alert(`
             ${languageText.computer}: ${inputComputer}
             ${languageText.player}: ${inputPlayer}
             ${languageText.drow}`);
-            choiceFigures();
-          } else {
-            isDrow = true;
-          }
+          choiceFigures();
+        } else {
+          isDrow = true;
         }
+      }
 
-        if (
-          (inputPlayer === FIGURES[0] && inputComputer === FIGURES[1]) ||
-          (inputPlayer === FIGURES[1] && inputComputer === FIGURES[2]) ||
-          (inputPlayer === FIGURES[2] && inputComputer === FIGURES[0])
-        ) {
-          alert(`
+      if (
+        (inputPlayer === FIGURES[0] && inputComputer === FIGURES[1]) ||
+        (inputPlayer === FIGURES[1] && inputComputer === FIGURES[2]) ||
+        (inputPlayer === FIGURES[2] && inputComputer === FIGURES[0])
+      ) {
+        alert(`
             ${languageText.computer}: ${inputComputer}
             ${languageText.player}: ${inputPlayer}
 
             ${languageText.win}`);
-          resultOne.player += 1;
-        } else {
-          alert(`
+        resultOne.player += 1;
+      } else {
+        alert(`
             ${languageText.computer}: ${inputComputer}
             ${languageText.player}: ${inputPlayer}
 
             ${languageText.loss}`);
-          resultOne.computer += 1;
-        }
-      };
-      runGame();
+        resultOne.computer += 1;
+      }
     };
   };
   window.gameRPS = game;
@@ -167,6 +163,36 @@ export let isGameRPSFinished = false;
         computer: 5,
       };
 
+      const getInputPlayer = () =>
+        prompt(`Введите число от 1 до ${result.player}`);
+
+      const isNumber = (num) => {
+        if (Number.isNaN(num) || !Number.isInteger(+num) ||
+          num > result.player || num < 1 || num === '') {
+          alert('Введите число!');
+          return false;
+        }
+        return true;
+      };
+
+      const isCountBall = () => {
+        if (result.player === 1) {
+          result.player -= 1;
+          result.computer += 1;
+        }
+
+        if (result.computer === 1) {
+          result.player += 1;
+          result.computer -= 1;
+        }
+      };
+
+      if (resultOne.player === 1) {
+        alert('Вы ходите первым');
+      } else {
+        alert('Бот ходит первым и загадывает число');
+      }
+
       return function start() {
         let randomInt;
         let isEven;
@@ -176,192 +202,170 @@ export let isGameRPSFinished = false;
         let isNumberComputer;
         let playAgain;
 
-        const getInputPlayer = () =>
-          prompt(`Введите число от 1 до ${result.player}`);
-
         const getInputComputer = () => {
           inputComputer = getRandomIntInclusive(1, result.computer);
         };
 
-        const isNumber = (num) => {
-          if (Number.isNaN(num) || !Number.isInteger(+num) ||
-            num > result.player || num < 1 || num === '') {
-            alert('Введите число!');
-            return false;
-          }
-          return true;
-        };
-
-        const isCountBall = () => {
-          if (result.player === 1) {
-            result.player -= 1;
-            result.computer += 1;
-          }
-
-          if (result.computer === 1) {
-            result.player += 1;
-            result.computer -= 1;
+        const isCancel = (val) => {
+          if (val === null) {
+            const exit = confirm(`Точно хотите выйти?`);
+            if (exit) {
+              return true;
+            } else {
+              start();
+            }
           }
         };
 
         if (resultOne.player === 1) {
-          alert('Вы ходите первым');
+          inputPlayer = getInputPlayer();
+
+          if (isCancel(inputPlayer)) return true;
+
+          if (!isNumber(inputPlayer)) {
+            start();
+          }
+
+          inputPlayer = +inputPlayer;
+
+          isNumberPlayer = inputPlayer % 2 === 0 ?
+            evenOrOdd[0] : evenOrOdd[1];
+
+          alert('Бот угадывает четное или нечетное число вы загадали');
+
+          randomInt = getRandomIntInclusive(0, 1);
+          isNumberComputer = evenOrOdd[randomInt];
+
+          if (isNumberPlayer === isNumberComputer) {
+            result.computer += inputPlayer;
+            result.player -= inputPlayer;
+
+            if (result.player < 1) {
+              result.player = 0;
+            }
+
+            alert(`
+              Бот угадал!
+              Количество шариков:
+  
+              У бота: ${result.computer}
+              У Вас: ${result.player}
+            `);
+          }
+
+          if (isNumberPlayer !== isNumberComputer) {
+            if (inputPlayer <= result.computer) {
+              result.computer -= inputPlayer;
+              result.player += inputPlayer;
+            } else {
+              result.player += result.computer;
+              result.computer -= result.computer;
+            }
+
+            if (result.computer < 1) {
+              result.computer = 0;
+            }
+
+            alert(`
+              Бот не угадал!
+              Количество шариков:
+
+              У Вас: ${result.player}
+              У бота: ${result.computer}
+            `);
+          }
+
+          resultOne.player = 0;
+          resultOne.computer = 1;
         } else {
-          alert('Бот ходит первым и загадывает число');
+          getInputComputer();
+          isEven = confirm('Число четное?');
+          isNumberPlayer = isEven === true ? evenOrOdd[0] : evenOrOdd[1];
+          isNumberComputer = inputComputer % 2 === 0 ?
+            evenOrOdd[0] : evenOrOdd[1];
+
+          if (isNumberPlayer === isNumberComputer) {
+            result.computer -= inputComputer;
+            result.player += inputComputer;
+
+            if (result.computer < 1) {
+              result.computer = 0;
+            }
+
+            alert(`
+              Вы угадали!
+              Количество шариков:
+
+              У бота: ${result.computer}
+              У Вас: ${result.player}
+            `);
+          }
+
+          if (isNumberPlayer !== isNumberComputer) {
+            if (inputComputer <= result.player) {
+              result.computer += inputComputer;
+              result.player -= inputComputer;
+            } else {
+              result.computer += result.player;
+              result.player -= result.player;
+            }
+
+            if (result.player < 1) {
+              result.player = 0;
+            }
+
+            alert(`
+              Вы не угадали!
+              Количество шариков:
+      
+              У Вас: ${result.player}
+              У бота: ${result.computer}
+            `);
+          }
+
+          resultOne.player = 1;
+          resultOne.computer = 0;
         }
 
-        const runGame = () => {
-          const isCancel = (val) => {
-            if (val === null) {
-              const exit = confirm(`Точно хотите выйти?`);
-              if (exit) {
-                return true;
-              } else {
-                runGame();
-              }
-            }
-          };
+        isCountBall();
 
-          if (resultOne.player === 1) {
-            inputPlayer = getInputPlayer();
+        if (result.computer < 1) {
+          alert(`Бот проиграл`);
+        }
 
-            if (isCancel(inputPlayer)) return true;
+        if (result.player < 1) {
+          alert(`Вы проиграли`);
+        }
 
-            if (!isNumber(inputPlayer)) {
-              runGame();
-            }
-
-            inputPlayer = +inputPlayer;
-
-            isNumberPlayer = inputPlayer % 2 === 0 ?
-              evenOrOdd[0] : evenOrOdd[1];
-
-            alert('Бот угадывает четное или нечетное число вы загадали');
-
-            randomInt = getRandomIntInclusive(0, 1);
-            isNumberComputer = evenOrOdd[randomInt];
-
-            if (isNumberPlayer === isNumberComputer) {
-              result.computer += inputPlayer;
-              result.player -= inputPlayer;
-
-              if (result.player < 1) {
-                result.player = 0;
-              }
-
-              alert(`
-                Бот угадал!
-                Количество шариков:
-  
-                У бота: ${result.computer}
-                У Вас: ${result.player}
-              `);
-            }
-
-            if (isNumberPlayer !== isNumberComputer) {
-              if (inputPlayer <= result.computer) {
-                result.computer -= inputPlayer;
-                result.player += inputPlayer;
-              } else {
-                result.player += result.computer;
-                result.computer -= result.computer;
-              }
-
-              if (result.computer < 1) {
-                result.computer = 0;
-              }
-
-              alert(`
-                Бот не угадал!
-                Количество шариков:
-  
-                У Вас: ${result.player}
-                У бота: ${result.computer}
-              `);
-            }
-
-            resultOne.player = 0;
-            resultOne.computer = 1;
-          } else {
-            getInputComputer();
-            isEven = confirm('Число четное?');
-            isNumberPlayer = isEven === true ? evenOrOdd[0] : evenOrOdd[1];
-            isNumberComputer = inputComputer % 2 === 0 ?
-              evenOrOdd[0] : evenOrOdd[1];
-
-            if (isNumberPlayer === isNumberComputer) {
-              result.computer -= inputComputer;
-              result.player += inputComputer;
-
-              if (result.computer < 1) {
-                result.computer = 0;
-              }
-
-              alert(`
-                Вы угадали!
-                Количество шариков:
-      
-                У бота: ${result.computer}
-                У Вас: ${result.player}
-              `);
-            }
-
-            if (isNumberPlayer !== isNumberComputer) {
-              if (inputComputer <= result.player) {
-                result.computer += inputComputer;
-                result.player -= inputComputer;
-              } else {
-                result.computer += result.player;
-                result.player -= result.player;
-              }
-
-              if (result.player < 1) {
-                result.player = 0;
-              }
-
-              alert(`
-                Вы не угадали!
-                Количество шариков:
-      
-                У Вас: ${result.player}
-                У бота: ${result.computer}
-                `);
-            }
-
-            resultOne.player = 1;
-            resultOne.computer = 0;
-          }
-
-          isCountBall();
-
-          if (result.computer < 1) {
-            alert(`Бот проиграл`);
-          }
-
-          if (result.player < 1) {
-            alert(`Вы проиграли`);
-          }
-
-          if (result.computer < 1 || result.player < 1) {
-            alert(`
+        if (result.computer < 1 || result.player < 1) {
+          alert(`
               Результат:
               Вы: ${result.player}
               Бот: ${result.computer}
             `);
-            playAgain = confirm('Хотите сыграть еще?');
-            if (playAgain) {
-              resultOne.player = 0;
-              resultOne.computer = 0;
-              result.player = 5;
-              result.computer = 5;
-              window.gameRPS()();
-            } else {
-              return false;
-            }
+          playAgain = confirm('Хотите сыграть еще?');
+          if (playAgain) {
+            resultOne.player = 0;
+            resultOne.computer = 0;
+            result.player = 5;
+            result.computer = 5;
+            window.gameRPS()();
+          } else {
+            return isGameMrcFinished = true;
           }
-          runGame();
-        };
-        runGame();
+        } else {
+          start();
+        }
+
+        if (isGameMrcFinished) {
+          return false;
+        }
+
+        if (!isGameRPSFinished) {
+          start();
+        } else {
+          return false;
+        }
       };
     };
 
